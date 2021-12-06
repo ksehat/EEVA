40840e878db6f3bcda8cbc5a2abc944c34ae60ed
K.Sehat <kan.sehat@gmail.com>
2021-12-06 12:07:57 +0330

    [change] multiprocessing is implemented at this version.

--- my_geneticalgorithm_multiprocess.py
+++ my_geneticalgorithm_multiprocess.py
@@ -1,3 +1,4 @@
+import concurrent.futures
 import random
 import math
 import pandas as pd
@@ -5,37 +6,52 @@ import numpy as np
 import copy
 from tqdm import tqdm
 
+
 class MyGeneticAlgorithm():
 
-    def __init__(self, config: dict, function, n_crossover: int=None,
-                 crossover_mode: str='rigid', population_size: int=100,
-                 run_iter=100, mutation_prob=.5, maximize: bool=True):
+    def __init__(self, config: dict, function, symbol, data_step, n_crossover: int = None,
+                 crossover_mode: str = 'rigid', population_size: int = 100,
+                 run_iter=100, mutation_prob=.8, maximize: bool = True):
         self.config = config
         self.variables_list = [k for k in config]
         self.params_list = [self.config[k] for k in self.config]
         self.n_crossover = n_crossover
         self.crossover_mode = crossover_mode
         self.function = function
+        self.symbol = symbol
+        self.data_step = data_step
         self.max_population_size = math.prod([len(self.config[k]) for k in self.config])
-        self.population_size = population_size if population_size<=self.max_population_size else self.max_population_size
+        self.population_size = population_size if population_size <= self.max_population_size else self.max_population_size
         self.run_iter = run_iter
         self.mutation_prob = mutation_prob
         self.maximize = maximize
+        self.all_populations = pd.DataFrame(columns=['members', 'score'])
 
     def generate_random_member(self):
         return [random.choice(self.params_list[i]) for i in range(len(self.params_list))]
 
     def evaluate(self, member):
-        output = self.function(member)
-        if isinstance(output,list):
-            if self.maximize: return 1e-20
-            else: return 1e20
+        if not self.all_populations.empty:
+            all_populations = list(self.all_populations['members'].values)
+            if member in all_populations:
+                index = all_populations.index(member)
+                return self.all_populations['score'][index]
+        output = self.function(member, self.symbol, self.data_step)
+        if not output:
+            if self.maximize:
+                return 1e-5
+            else:
+                return 1e5
         try:
-            if output < 0: return 1e-20
-            else: return output
+            if output < 0:
+                return 1e-5
+            else:
+                return output
         except:
-            if self.maximize: return 1e-20
-            else: return 1e20
+            if self.maximize:
+                return 1e-5
+            else:
+                return 1e5
 
     def crossover(self, first_parent, second_parent, n_crossover=None, crossover_mode=None):
         if n_crossover is None:
@@ -73,91 +89,128 @@ class MyGeneticAlgorithm():
         mutation_point = random.randrange(len(member))
         params_list = copy.deepcopy(self.params_list[mutation_point])
         params_list.remove(member[mutation_point])
-        member[mutation_point] = random.sample(params_list,k=1)[0]
+        member[mutation_point] = random.sample(params_list, k=1)[0]
         return member
 
-    def run(self, keep_frac: float=10, crossover_frac: float=10, mutate_frac: float=35):
-        n_keep = math.floor((keep_frac/100)*self.population_size)
+    def run(self, keep_frac: float = 3, crossover_frac: float = 3, mutate_frac: float = 3):
+        n_keep = math.floor((keep_frac / 100) * self.population_size)
         n_crossover = math.floor((crossover_frac / 100) * self.population_size)
         n_mutate = math.floor((mutate_frac / 100) * self.population_size)
         n_random = self.population_size - n_keep - n_mutate - n_crossover
         pbar = tqdm(total=self.run_iter)
         # region generate random population
-        population=[]
-        iter=1
+        population = []
+        iter = 1
         while iter <= self.population_size:
             member = self.generate_random_member()
             if member not in population:
                 population.append(member)
-                iter+=1
+                iter += 1
         # endregion
         # region Evaluate each member and make member/score dataframe
-        member_list=[]
-        score_list=[]
+        member_list = []
+        score_list = []
         for member in population:
             member_list.append(member)
-            score_list.append(self.evaluate(member))
+        with concurrent.futures.ProcessPoolExecutor(max_workers=14) as executor:
+            for member, score in zip(member_list, executor.map(self.evaluate, member_list)):
+                score_list.append(score)
         population_score_df = pd.DataFrame({
-            'members':member_list,
-            'score':score_list
+            'members': member_list,
+            'score': score_list
         })
         population_score_df.sort_values('score', inplace=True, ignore_index=True, ascending=False)
-        new_population_score_df = pd.DataFrame({'members': [list(range(3))]*self.population_size,
-                                                'score': list(np.zeros(self.population_size))
-                                                })
+        self.all_populations = copy.deepcopy(population_score_df)
+        new_population_score_df = pd.DataFrame(
+            {'members': [list(np.full(len(self.variables_list), np.nan))] *
+                        self.population_size,
+             'score': list(np.full([self.population_size],
+                                   np.nan))
+             })
+        new_population_score_df[:n_keep] = copy.deepcopy(population_score_df[:n_keep])
         # endregion
         # region iter until required score is satisfied
-        run_iter=1
+        run_iter = 1
         while run_iter < self.run_iter:
             # region crossover
             iter_cross = 0
             while iter_cross < n_crossover:
-                first_parent  = random.choices(population_score_df['members'], weights=population_score_df['score'])
-                second_parent = random.choices(population_score_df['members'], weights=population_score_df['score'])
-                child = copy.deepcopy(self.crossover(first_parent[0],second_parent[0]))
-                if child not in list(new_population_score_df['members'][:n_keep+iter_cross]):
-                    new_population_score_df.at[n_keep+iter_cross, 'members'] = copy.deepcopy(child)
-                    new_population_score_df.at[n_keep+iter_cross, 'score'] = copy.deepcopy(self.evaluate(child))
-                    iter_cross+=1
+                first_parent = random.choices(population_score_df['members'],
+                                              weights=population_score_df['score'])
+                second_parent = random.choices(population_score_df['members'],
+                                               weights=population_score_df['score'])
+                child = copy.deepcopy(self.crossover(first_parent[0], second_parent[0]))
+                if child not in list(new_population_score_df['members'][:n_keep + iter_cross]):
+                    new_population_score_df.at[n_keep + iter_cross, 'members'] = copy.deepcopy(
+                        child)
+                    # new_population_score_df.at[n_keep + iter_cross, 'score'] = copy.deepcopy(
+                    #     self.evaluate(child))
+                    iter_cross += 1
             # endregion
             # region mutation
             iter_mut = 0
             while iter_mut < n_mutate:
-                mut_member = copy.deepcopy(random.choices(population_score_df['members'], weights=population_score_df['score'])[0])
+                mut_member = copy.deepcopy(random.choices(population_score_df['members'],
+                                                          weights=population_score_df['score'])[0])
                 child = copy.deepcopy(self.mutate(mut_member))
                 if child == None: continue
-                if child not in list(new_population_score_df['members'][:n_keep+n_crossover+iter_mut]):
-                    new_population_score_df.at[n_keep+n_crossover+iter_mut, 'members'] = copy.deepcopy(child)
-                    new_population_score_df.at[n_keep+n_crossover+iter_mut, 'score'] = copy.deepcopy(self.evaluate(child))
-                    iter_mut+=1
+                if child not in list(
+                        new_population_score_df['members'][:n_keep + n_crossover + iter_mut]):
+                    new_population_score_df.at[
+                        n_keep + n_crossover + iter_mut, 'members'] = copy.deepcopy(child)
+                    # new_population_score_df.at[
+                    #     n_keep + n_crossover + iter_mut, 'score'] = copy.deepcopy(
+                    #     self.evaluate(child))
+                    iter_mut += 1
             # endregion
             # region random
             iter_rand = 0
             while iter_rand < n_random:
                 random_member = copy.deepcopy(self.generate_random_member())
-                if random_member not in list(new_population_score_df['members'][:n_keep+n_crossover+n_mutate+iter_rand]):
-                    new_population_score_df.at[n_keep+n_crossover+n_mutate+iter_rand, 'members'] = copy.deepcopy(random_member)
-                    new_population_score_df.at[n_keep+n_crossover+n_mutate+iter_rand, 'score'] = copy.deepcopy(self.evaluate(random_member))
-                    iter_rand+=1
+                if random_member not in list(new_population_score_df['members'][
+                                             :n_keep + n_crossover + n_mutate + iter_rand]):
+                    new_population_score_df.at[
+                        n_keep + n_crossover + n_mutate + iter_rand, 'members'] = copy.deepcopy(
+                        random_member)
+                    # new_population_score_df.at[
+                    #     n_keep + n_crossover + n_mutate + iter_rand, 'score'] = copy.deepcopy(
+                    #     self.evaluate(random_member))
+                    iter_rand += 1
             # endregion
-            new_population_score_df.sort_values(['score'], inplace=True, ignore_index=True, ascending=False)
+            score_list = []
+
+            with concurrent.futures.ProcessPoolExecutor(max_workers=14) as executor:
+                for member, score in zip(new_population_score_df['members'][n_keep:],
+                                         executor.map(self.evaluate, new_population_score_df['members'][n_keep:])):
+                    score_list.append(score)
+            new_population_score_df['score'][n_keep:] = score_list
+            new_population_score_df.sort_values(['score'], inplace=True, ignore_index=True,
+                                                ascending=not self.maximize)
             population_score_df = copy.deepcopy(new_population_score_df)
-            run_iter+=1
+            self.all_populations = pd.concat([self.all_populations, population_score_df],
+                                             ignore_index=True)
+            run_iter += 1
             pbar.update(run_iter)
         # endregion
         return new_population_score_df[:20]
 
 
-
 # ali = {
-#     'fast_window':[3,4,5,6,7,8,9],
-#     'slow_window':[10,20,30,40,50,60],
-#     'sign_window':[130,140,150,160],
+#     'fast_window': [3, 4, 5, 6, 7, 8, 9],
+#     'slow_window': [10, 20, 30, 40, 50, 60],
+#     'sign_window': [130, 140, 150, 160],
 # }
 #
-# def f(X):
-#     return (X[0]+X[1]+X[2])
 #
-# ga = MyGeneticAlgorithm(config=ali,function=f,run_iter=5, population_size=40, n_crossover=3, crossover_mode='random')
-# best_params=ga.run()
-# print(best_params)
+# def f(X, symbol, data_step):
+#     return (X[0] + X[1] + X[2])
+#
+#
+# ga = MyGeneticAlgorithm(config=ali, function=f, run_iter=5, population_size=250, n_crossover=3,
+#                         crossover_mode='random', symbol='ETHUSDT', data_step='30m')
+# def main():
+#     best_params = ga.run(keep_frac = 20, crossover_frac = 10, mutate_frac = 10)
+#     print(best_params)
+#
+# if __name__ == '__main__':
+#     main()
