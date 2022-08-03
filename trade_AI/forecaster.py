e28b2f9f881976adebf710d8ac2dede151e829ff
ksehat <kan.sehat@gmail.com>
2022-08-04 02:50:53 +0430

    profit and win rate formula is added.

--- trade_AI/forecaster.py
+++ trade_AI/forecaster.py
@@ -1,14 +1,14 @@
 import pandas as pd
-
+import math
 from sklearn.ensemble import RandomForestClassifier as rfc
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import confusion_matrix
 
-df = pd.read_csv('df.csv').dropna()
-
-train = df.iloc[:-4000,:]
-val = df.iloc[-4000:-2000,:]
-test = df.iloc[-2000:,:]
+df = pd.read_csv('df_15m.csv').dropna()
+train_end = 8640*2
+train = df.iloc[:-train_end,:]
+# val = df.iloc[-4000:-2000,:]
+test = df.iloc[-train_end:,:]
 x_train = train.iloc[:, :-1]
 y_train = train.iloc[:, -1]
 # x_val = val[:][:-1]
@@ -17,10 +17,13 @@ x_test = test.iloc[:, :-1]
 y_test = test.iloc[:, -1]
 
 
-
-# model = rfc(class_weight={0:0.01,1:100})
-# model.fit(x_train, y_train)
-# y_pred = model.predict(x_test)
-# print(accuracy_score(y_test, y_pred))
-# print('===========')
-# print(confusion_matrix(y_test, y_pred))
+model = rfc(class_weight={0:0.001,1:100})
+model.fit(x_train, y_train)
+y_pred = model.predict(x_test)
+print(accuracy_score(y_test, y_pred))
+print('===========')
+result_cm = confusion_matrix(y_test, y_pred)
+win_rate = result_cm[1][1]/(result_cm[0][1] + result_cm[1][1])
+profit = math.pow((1+0.015-0.0012),result_cm[1][1])*math.pow((1-0.005-0.0012),result_cm[0][1])
+print('win rate is:',win_rate)
+print('profit is:',profit)
